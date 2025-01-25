3a1be9b07edf99e830afefc703bbc07e462d9f6b
jdmartens <jeremydmartens@gmail.com>
2025-01-24 21:05:24 -0600

    init commit

---
+++ lambda_function.py
@@ -0,0 +1,39 @@
+import os
+import boto3
+from PIL import Image
+import io
+
+s3_client = boto3.client('s3')
+
+def lambda_handler(event, context):
+    # Get environment variables
+    output_bucket = os.environ['DES_BUCKET']
+    width = int(os.environ['WIDTH'])
+    height = int(os.environ['HEIGHT'])
+
+    # Get the source bucket and object key from the event
+    print('$$$$$$ event',event, context)
+    source_bucket = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
+    
+    # Download the image from S3
+    response = s3_client.get_object(Bucket=source_bucket, Key=object_key)
+    image_content = response['Body'].read()
+    
+    # Resize the image
+    with Image.open(io.BytesIO(image_content)) as img:
+        resized_img = img.resize((width, height))
+        
+        # Save the resized image to a buffer
+        buffer = io.BytesIO()
+        resized_img.save(buffer, format=img.format)
+        buffer.seek(0)
+    
+    # Upload the resized image to the output bucket
+    print('$$$$$$ output_bucket', output_bucket, object_key)
+    s3_client.put_object(Bucket=output_bucket, Key=f"resized_{object_key}", Body=buffer)
+    
+    return {
+        'statusCode': 200,
+        'body': 'Image resized successfully'
+    }
